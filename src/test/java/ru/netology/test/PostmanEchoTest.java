acff1cec1a1fade8de9d3d06ba902f91e196d6f6
Tatiana Lyapina <antipts@gmail.com>
2022-08-03 01:32:48 +0300

    Test fixed

--- src/test/java/ru/netology/test/PostmanEchoTest.java
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Postman Echo"))
+                .body("data", equalTo("Postman Echo test"))
         ;
     }
 }
